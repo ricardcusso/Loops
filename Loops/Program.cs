fc6f908508234f9643ed7e7262a8defabe0739fd
Ricard <ricard.cusso@gmail.com>
2020-12-08 22:03:25 +1000

    Second Exercise

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,12 +6,26 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            for(var i = 0; i <= 10; i++)
+            //for (var i = 0; i <= 10; i++)
+            //{
+            //    //Console.WriteLine(i);
+
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            var name = "Ricard Cusso";
+
+            //for (var i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            foreach (var character in name)
             {
-                if(i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine(character);
             }
         }
     }
